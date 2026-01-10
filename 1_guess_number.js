231ab270e45a8eecc0d2b74eb94d36913a55362b
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-01-10 21:28:17 +0500

    alpha v0.0.5.0

--- 1_guess_number.js
+++ 1_guess_number.js
@@ -1,33 +1,100 @@
-let range = 10;
+let range;
 let number;
 let numberAttempts;
+let game;
+let minNumber;
+let maxNumber;
+
+simulateClick("#modePC");
+simulateClick("#modeB");
+
 
 document.getElementById("customRangeID").addEventListener('input', function(e) {
-    if(this.value > 0){
-        document.getElementById("customBtnID").removeAttribute("disabled");
-    } else {
-        document.getElementById("customBtnID").disabled = "disabled"
-    }
+    range = this.value > 1 ? this.value : 2;
+    infoChange();
+    newNumber()
 })
 
-function newNumber(){
-    number = Math.ceil(Math.random()*range);
-    document.getElementById("checkNumberBtnID").removeAttribute("disabled");
-    toChangeText("taskID", "Компьютер загадал число от 1 до " + range + ". Угадай его!")
-    numberAttempts = Math.ceil(Math.log2(range))
-    toChangeText("numberAttemptsID", "Количество попыток: " + numberAttempts);
-    toChangeText("messageID", "");
-    document.getElementById("numberToCheckID").value = "";
-    checkNumber.count = 0;
+function newNumber(znak){
+    toChangeText("messageID", game);
+    if(game == "pc"){
+        number = Math.ceil(Math.random()*range);
+        document.getElementById("checkNumberBtnID").removeAttribute("disabled");
+        toChangeText("messageID", "");
+        document.getElementById("numberToCheckID").value = "";
+        numberAttempts = Math.ceil(Math.log2(range))
+        checkNumber.count = 0;
+        toChangeText("numberAttemptsID", "Количество попыток: " + numberAttempts);
+    } else if(game == "man"){
+        if(znak == "more"){
+            minNumber = number;
+        }else if(znak == "less"){
+            maxNumber = number;
+        }else{
+            minNumber = 1;
+            maxNumber = range;
+            number = Math.ceil(Math.random()*range);
+        }
+        while(number <= minNumber || number >= maxNumber){
+            number = Math.ceil(Math.random()*range);
+        }
+        toChangeText("messageID", "Число компьютера: " + number);
+        numberAttempts--
+        toChangeText("numberAttemptsID", "Количество попыток: " + numberAttempts);
+        if(numberAttempts < 0){
+            toChangeText("messageID", "Компьютер не смог угадать ваше число!");
+            toChangeText("numberAttemptsID", "");
+        }
+    }
 }
 
-function gameMode(r){
-    range = r;
-    newNumber();
-    toSeeable("gameID");
+function gameMode(mode){
+
+    const modes = ["modePC", "modeMan"];
+
+    for(let i = 0; i < modes.length; i++){
+        document.getElementById(modes[i]).removeAttribute("disabled");
+    } 
+    document.getElementById(mode.id).disabled = "disabled";
+    if(mode.id == "modePC"){
+        game = "pc";
+        toHide("gameManID");
+        toSeeable("gamePCID");
+    } else if(mode.id == "modeMan"){
+        game = "man";
+        toHide("gamePCID");
+        toSeeable("gameManID");
+    }
+    infoChange();
 }
 
-function checkNumber(){
+function infoChange(){
+    if(game == "pc"){
+        toChangeText("taskID", "Компьютер загадал число от 1 до " + range + ". Угадай его!")
+    } else if(game == "man"){
+        toChangeText("taskID", "Вам необходимо загадать число от 1 до " + range + ". Компьютер будет угадывать!")
+    }
+}
+
+function difficulty(mode){
+    const modes = ["modeA", "modeB", "modeC", "modeD", "modeE"];
+    for(let i = 0; i < modes.length; i++){
+        document.getElementById(modes[i]).removeAttribute("disabled");
+    } 
+    document.getElementById(mode.id).disabled = "disabled";
+    
+    
+    if(mode.id == "modeE"){
+        toSeeable("customRangeID");
+    }else {
+        range = Number(mode.value);
+        newNumber();
+    }
+    infoChange();
+}
+
+function checkNumber(mode){
+    if(mode == "gamePC"){
     const numberToCheck  = Math.round(document.getElementById("numberToCheckID").value);
     if(numberToCheck <= range){
         checkNumber.count = (checkNumber.count || 0) + 1;
@@ -50,4 +117,21 @@ function checkNumber(){
     } else{
         toChangeText("messageID", "Число должно быть в диапозоне от 1 до " + range + "!");
     }
+    } else if(mode == "gameMan"){
+        const gameManBtns = ["moreID", "lessID", "pcRightID"];
+        for(let i = 0; i < gameManBtns.length; i++){
+            document.getElementById(gameManBtns[i]).removeAttribute("disabled");
+        }
+            numberAttempts = Math.ceil(Math.log2(range))+2
+        newNumber();
+    }
 }
+
+function pcRightNumber(){
+    const gameManBtns = ["moreID", "lessID", "pcRightID"];
+        for(let i = 0; i < gameManBtns.length; i++){
+            document.getElementById(gameManBtns[i]).disabled = "disabled";
+        } 
+    toChangeText("messageID", "Компьютер угадал ваше число:" + number + "!" );
+}
+
